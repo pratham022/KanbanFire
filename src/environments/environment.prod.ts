863c3adb7121f88fac0aa91b8297d79c6607b005
pratham022 <pthombre657@gmail.com>
2021-07-18 14:58:21 +0530

    Delpoy complete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2EXK26lgo0XCR3L_DLnzMAXNhMgee80U",
+    authDomain: "utility-app-343d6.firebaseapp.com",
+    projectId: "utility-app-343d6",
+    storageBucket: "utility-app-343d6.appspot.com",
+    messagingSenderId: "229741744533",
+    appId: "1:229741744533:web:ca43926d365a81c0a1a69c",
+    measurementId: "G-VGNHPLG2W3"
+  }
 };
